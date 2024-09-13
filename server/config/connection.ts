26dee5fd6a5923e6f02262927997e0223500fea7
Isaiah Capers <isaiah.capers@outlook.com>
2024-09-13 13:55:01 -0400

    Initial Commit

---
+++ server/config/connection.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Sequelize } from 'sequelize';
+
+// TODO: Create sequelize connection
+
+const sequelize = '';
+
+export default sequelize;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
