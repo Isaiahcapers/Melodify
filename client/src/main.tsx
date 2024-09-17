fe1f1272c85307ff0b6fad1d6f6a187c7a669af7
ZapApriendo <isaiah.capers@outlook.com>
2024-09-16 20:25:20 -0400

    pages folder with pathing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App.tsx';
+
+import ErrorPage from './pages/ErrorPage.tsx';
+import HomePage from './pages/Home.tsx';
+import Login from './pages/Login.tsx';
+import Playlist from './pages/Playlist';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      }, 
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/playlist',
+        element: <Playlist />
+      }, 
+    ]
+  }
+])
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
