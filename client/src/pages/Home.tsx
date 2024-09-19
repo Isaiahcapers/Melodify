fffff6433ab601dab9a3bf2c6bcdc9f6a0a5424c
ZapApriendo <isaiah.capers@outlook.com>
2024-09-19 19:24:37 -0400

    playlist work

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -27,7 +27,7 @@ const Home = () => {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:3000/");
+    params.append("redirect_uri", "http://localhost:3000/home");
     params.append("scope", "user-read-private user-read-email");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
@@ -61,7 +61,7 @@ const Home = () => {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:3000/");
+    params.append("redirect_uri", "http://localhost:3000/home");
     params.append("code_verifier", verifier!);
     console.log(params);
 
