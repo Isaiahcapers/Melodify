39952d4a3688894057a83749ab37c21dc9b94a25
Isaiah Capers <isaiah.capers@outlook.com>
2024-09-22 14:24:31 -0400

    fixed navigaition spacing

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,22 +1,28 @@
 import '../CSS/Footer.css';
-import { Shuffle,PlayCircle,ArrowRightCircle,ArrowLeftCircle,ArrowClockwise } from 'react-bootstrap-icons';
+import { Shuffle,PlayCircle,ArrowRightCircle,ArrowLeftCircle,ArrowClockwise,VolumeUp,VolumeDown,VolumeMute } from 'react-bootstrap-icons';
 
 export default function Footer() {
 
     return (
         <footer className="footer">
             <div className="footer-details">
+                <img src="" alt="" />
+                <div className='footer-song-info'>
+                <h4></h4>
                 <p>details</p>
+                </div>
             </div>
             <div className="footer-controls">
                 <Shuffle />
                 <ArrowLeftCircle />
-                <PlayCircle fontSize="large"/>
+                <PlayCircle />
                 <ArrowRightCircle />
                 <ArrowClockwise />
             </div>
             <div className="footer-volume">
-                <p>volume</p>
+                <VolumeMute/>
+                <VolumeDown/>
+                <VolumeUp/>
             </div>
         </footer> 
     );
