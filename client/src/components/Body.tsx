2aa6bdc1a8bcbfdab4f095d092fb814e1ee607ad
Isaiah Capers <isaiah.capers@outlook.com>
2024-09-23 02:24:23 -0400

    header component complete

--- client/src/components/Body.tsx
+++ client/src/components/Body.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../CSS/Body.css';
-
+import Header from './Header';
 function Body() {
+  
   return (
     <div className ="body">
+      <Header />
       <h1>I'm a body</h1>
     </div>
   );
