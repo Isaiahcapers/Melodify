1485063fbfdd196294dcdf5b7a6f8f6bb4a45cca
Justin <j.hebenstreit25@gmail.com>
2024-09-23 20:05:44 -0500

    fixed bugs

--- server/src/routes/api/userRoutes.ts
+++ server/src/routes/api/userRoutes.ts
@@ -7,7 +7,6 @@ const router = Router();
  * GET /users - Fetch all users (dummy route)
  */
 router.get('/', (_req, res) => {
-router.get('/', (_req, res) => {
   res.json({ users: [{ id: 1, username: 'admin' }] });
 });
 
