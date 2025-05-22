0839f0a8cf0188f97ae9a6e45aaf76cc26804f9b
Justin <j.hebenstreit25@gmail.com>
2025-05-22 13:08:15 -0500

    separate project and moved front end out of client folder

--- server/src/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-import { Router } from 'express';
-//import { loginRouter } from './authRoutes.js';
-import { authRoutes } from './authRoutes.js'; // Importing authRoutes
-const router = Router();
-router.use('/auth', authRoutes); // Mounting the login router on the /api path
-export default router;
